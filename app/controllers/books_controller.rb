ba764f957fce9150bf670f527deabf4c2dfdf35e
mayo25 <m.lupicia@gmail.com>
2021-10-21 10:16:11 +0000

    [finish]課題の終了

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+  
+  def index
+     @books = Book.all
+     @book = Book.new
+  end
+
+  def show
+     @book = Book.find(params[:id])
+  end
+
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+   
+  end
+  
+  def create
+     @book = Book.new(book_params)
+     @books=Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      render :index
+    end
+  end
+  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "Book was successfully update."
+   else
+      render :edit
+   end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+   if @book.destroy
+    redirect_to books_path(@book.id), notice: "Book was successfully destroy."
+   else
+      render :index
+   end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
+
+
+
+
+
+
